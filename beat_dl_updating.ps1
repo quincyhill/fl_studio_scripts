8815a0a7c3567907a108667b1759556494696882
quincyhill <therealquincyhill@gmail.com>
2021-09-02 09:44:16 -0700

    Updated the README, and added a license

--- beat_dl_updating.ps1
+++ beat_dl_updating.ps1
@@ -1,6 +1,6 @@
+# STILL ISNT FINSIHED DONT USE THIS YET
 # Gets a url downloads the formatted mp3 and moves it to the beat directory for FL Studio
 # Also could add a more robust way to clip off the end of the url list so I dont have to copy upto the &List Value
-
 
 # $youtube_dl_beat_path = "C:\Program Files\Image-Line\FL Studio 20\Data\Patches\Packs\Downloaded Beats\%(title)s prod[%(uploader)s].%(ext)s"
 $beatDataPath2 = "C:\Users\12242\Documents\MusicLog\beatlist.csv"
@@ -23,20 +23,11 @@ for ($i = 0; $i -lt 100; $i++) {
 # number of lines
 $numberOfLines = Get-Content -Path $beatDataPath
 
-
-
 Write-Output $numberOfLines.Count
 
 # This opens the file in an app
 # Invoke-Item -Path $beatDataPath
-
-
-
-
 # # simply open the file after the write
-
-
 # $url_link = Read-Host "Enter a youtube URL"
-
 # Just for testing purposes I will comment this out and figure out some file io with powershell
 # youtube-dl.exe -o $youtube_dl_beat_path -x --audio-format mp3 $url_link > log.txt
